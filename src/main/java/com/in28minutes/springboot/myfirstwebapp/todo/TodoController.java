6194dba63d6765de068a89f0432a3c088fadc2a1
Cactus <ysjungman@gmail.com>
2023-07-06 19:21:24 +0900

    103_페이지검증-alert-js활용

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -28,7 +32,7 @@ public class TodoController {
 		return "listTodos";
 	}
 
-	// POST
+	// GET
 	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage(ModelMap model) {
 		String username = (String)model.get("name");
@@ -38,13 +42,22 @@ public class TodoController {
 		return "todo";
 	}
 
-	// GET
+	// POST
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewTodo(ModelMap model, Todo todo) {
+	public String addNewTodo(ModelMap model, 
+			@Valid Todo todo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("message", "10String up plese");
+			return "todo";
+		}
+		
 		String username = (String) model.get("name");
 		todoService.addTodo(username, todo.getDescription(), 
 			LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
+	
+	
 
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,28 +1,33 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 
 <html>
 <head>
 <link href="webjars/bootstrap/5.3.0/css/bootstrap.min.css"
 	rel="stylesheet">
-<title>add Todo page</title>
+<title>Add Todo page</title>
+<script>
+	var message = "${message}";
+	if (message) {
+		alert(message);
+	}
+</script>
 </head>
 <body>
 	<div class="container">
 		<h1>Enter Todo Details</h1>
 		<form:form method="POST" modelAttribute="todo">
-			Description: <form:input type="text" path="description" 
-											required="required"/>
-									 <form:input type="hidden" path="id"/>				
-									 <form:input type="hidden" path="done"/>				
-							
-			<input type="submit" class="btn btn-success"/>
+			Description: <form:input type="text" path="description"
+				required="required" />
+			<form:input type="hidden" path="id" />
+			<form:input type="hidden" path="done" />
+
+			<input type="submit" class="btn btn-success" />
 		</form:form>
-		
+
 	</div>
 	<script src="webjars/bootstrap/5.3.0/js/bootstrap.min.js"></script>
 	<script src="webjars/jquery/3.6.4/jquery.min.js"></script>
-	<!-- <script src="http://code.jquery.com/jquery-latest.min.js"></script> -->
 </body>
 </html>
