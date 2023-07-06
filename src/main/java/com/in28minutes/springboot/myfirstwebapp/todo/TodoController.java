a505e0fdf86a0e029300834947a25b59466f0bcb
Cactus <ysjungman@gmail.com>
2023-07-06 16:59:16 +0900

    101_Todo-Add-2

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -27,16 +29,18 @@ public class TodoController {
 		return "listTodos";
 	}
 
-	//POST
-	@RequestMapping(value ="add-todo", method = RequestMethod.GET)
+	// POST
+	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage() {
 		return "todo";
 	}
 
-	//GET
-	@RequestMapping(value ="add-todo", method = RequestMethod.POST)
-	public String addNewTodo() {
-		
+	// GET
+	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		String username = (String) model.get("name");
+		todoService.addTodo(username, description, 
+				LocalDate.now().plusYears(1), false);
 		return "redirect:list-todos";
 	}
 
